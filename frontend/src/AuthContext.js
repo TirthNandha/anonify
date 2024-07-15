ccb08d81bd0e72ceacc1b577e612c974d9838be7
TirthNandha <ntirth15@gmail.com>
2024-07-15 15:32:07 +0530

    minor changes

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
       setUser(response.data.user);
     } catch (error) {
       console.error("Error validating token:", error.response ? error.response.data : error.message);
-      // logout();
+      logout();
     }
   };
 
