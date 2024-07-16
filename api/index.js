cb4cd8f3f0d422f5ffec427220811c32d4e1cfdb
TirthNandha <ntirth15@gmail.com>
2024-07-16 23:04:00 +0530

    cors updated

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const { createServer } = require('http');
 const app = express();
 
 const corsOptions = {
-  origin: "https://localhost:3000",
+  origin: "https://anonify-5rcdgap5k-tirthnandhas-projects.vercel.app",
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }
